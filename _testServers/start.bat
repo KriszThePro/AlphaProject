e498b84271f4ea04d95412210491e8524c905f03
KriszThePro <szollosi.krisztian.robert@gmail.com>
2022-06-15 22:38:44 +0200

    x

--- _testServers/start.bat
+++ _testServers/start.bat
@@ -3,7 +3,6 @@
 :: change CHCP to UTF-8
 CHCP 65001
 
-set version=""
 set /p version="Give a Minecraft version to run! "
 if not exist %version%\spigot-%version%.jar (
 	echo This version of server not found. Exiting.
@@ -13,19 +12,19 @@ if not exist %version%\spigot-%version%.jar (
 
 cd %version%
 
-:start
+:_start
 
 cls
 
-java -Xms256M -Xmx2G -DIReallyKnowWhatIAmDoingISwear=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar spigot-%version%.jar nogui
-
 title Server (version %version%)
+
+java -Xms256M -Xmx2G -DIReallyKnowWhatIAmDoingISwear=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar spigot-%version%.jar nogui
 
 set choice=""
 set /p choice="Do you want to restart? Press 'y' or 'Y' and 'Enter' to restart! "
 if not "%choice%" == "" (
-	if "%choice:~0,1%" == "y" goto start
-	if "%choice:~0,1%" == "Y" goto start
+	if "%choice:~0,1%" == "y" goto _start
+	if "%choice:~0,1%" == "Y" goto _start
 )
 
 :_exit
